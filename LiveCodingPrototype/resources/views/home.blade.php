caaa330ee11aaf6791f4600da010affc1bff79df
yasmineh-d <yassminehadad75@gmail.com>
2025-10-20 11:04:20 +0100

    done

--- LiveCodingPrototype/resources/views/home.blade.php
+++ LiveCodingPrototype/resources/views/home.blade.php
@@ -54,7 +54,7 @@
         <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.</p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
           <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
-          <a href="#" class="text-sm/6 font-semibold text-gray-900">Learn more <span aria-hidden="true">→</span></a>
+          <a href="#" class="text-sm/6 font-semibold text-gray-900">Learn  more <span aria-hidden="true">→</span></a>
         </div>
       </div>
     </div>
